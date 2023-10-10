01b372fb78641076bde049c2fe3697b8b1f46910
Alexandre Tavares <alexandre.tavares@wundermanthompson.com>
2023-10-10 15:20:47 +0100

    limit trending images and UI changes

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://api.giphy.com/v1/gifs/';
 export const API_KEY = 'api_key=qANODRlaZcrZr25RYKCLbpAG61Znj0dK';
 export const TIMEOUT_SEC = 15;
 export const RES_PER_PAGE = 20;
+export const TRENDING_RES = 10;
